601f22b0c3a3cb67298aef762c41e8e76b8d7382
Nate W <54341514+natew100@users.noreply.github.com>
2023-06-23 18:00:47 -0400

    cfg fixes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, make_response, render_template, redirect, url_for, request
+import os
+import logging
+from flask import Flask, jsonify, make_response, render_template, redirect, url_for, request
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
@@ -7,42 +9,38 @@ from flask_paginate import Pagination, get_page_args
 from bson.objectid import ObjectId
 from bson.errors import InvalidId
 from gridfs import GridFS
-from io import BytesIO
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key'
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+try:
+    app = Flask(__name__)
+    load_dotenv()
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    login_manager = LoginManager(app)
+    login_manager.login_view = 'login'
+    bcrypt = Bcrypt(app)
+    mongodb_uri = os.environ.get('MONGODB_URI')
+    log_folder = 'logs'
+    log_file = 'log.log'
+    log_path = os.path.join(log_folder, log_file)
+    os.makedirs(log_folder, exist_ok=True)
+    logging.basicConfig(filename=log_path, level=logging.INFO)
+except Exception as e:
+    logging.error(f"Error Starting Flask App: {str(e)}")
 
-bcrypt = Bcrypt(app)
-
-client = MongoClient("mongodb+srv://nathanwolf100:s2UHWRzrxdSM8v6C@admin-panel.dvrgnkx.mongodb.net/", 
-                    tls=True, tlsAllowInvalidCertificates=True, serverSelectionTimeoutMS=5000)
-
-db = client['wcre_panel']
-users = db['users']
-listings = db['listings']
-fs = GridFS(db)
-
-@login_manager.user_loader
-def load_user(username):
-    u = users.find_one({"username": username})
-    return User(u['username'], u['password'], u['role'], u['fullname'], u.get('profile_picture_url')) if u else None
+try:
+    client = MongoClient(mongodb_uri, tls=True, tlsAllowInvalidCertificates=True, serverSelectionTimeoutMS=5000)
+    db = client['wcre_panel']
+    users = db['users']
+    listings = db['listings']
+    fs = GridFS(db)
+    logging.info("Connected to MongoDB successfully")
+except Exception as e:
+    logging.error(f"Error connecting to MongoDB: {str(e)}")
 
 @app.route('/')
 def login_page():
     return redirect(url_for('login'))
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         user = load_user(request.form['username'])
-#         if user and bcrypt.check_password_hash(user.password, request.form['password']):
-#             login_user(user)
-#             return redirect(url_for('home'))
-#         return render_template('login.html', error='Invalid username or password')
-#     return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -50,20 +48,22 @@ def login():
         user = load_user(request.form['username'])
         if user and bcrypt.check_password_hash(user.password, request.form['password']):
             login_user(user)
-            return redirect(url_for('view_listings'))  # Change the redirect URL
+            log_message = f"User {user.username} logged in successfully"
+            logging.info(log_message)
+            return redirect(url_for('view_listings'))
+        logging.info("Invalid username or password")
         return render_template('login.html', error='Invalid username or password')
     return render_template('login.html')
 
-
-# @app.route('/home')
-# @login_required
-# def home():
-#     return render_template('home.html') if current_user.is_authenticated else redirect(url_for('login'))
+@login_manager.user_loader
+def load_user(username):
+    u = users.find_one({"username": username})
+    return User(u['username'], u['password'], u['role'], u['fullname'], u.get('profile_picture_url')) if u else None
 
 @app.route('/home')
 @login_required
 def home():
-    return redirect('view_listings') if current_user.is_authenticated else redirect(url_for('login'))
+    return redirect(url_for('view_listings')) if current_user.is_authenticated else redirect(url_for('login'))
 
 @app.route('/logout')
 @login_required
@@ -93,15 +93,14 @@ def view_listings():
     if current_user.is_authenticated:
         page, per_page, _ = get_page_args(page_parameter='page', per_page_parameter='per_page')
         per_page = 12
-        total, listings_data = (listings.count_documents({}), 
+        total, listings_data = (listings.count_documents({}),
                                 listings.find().skip((page-1)*per_page).limit(per_page)) if current_user.role == 'Admin' else (
-                                    listings.count_documents({"brokers": {"$in": [current_user.fullname]}}), 
+                                    listings.count_documents({"brokers": {"$in": [current_user.fullname]}}),
                                     listings.find({"brokers": {"$in": [current_user.fullname]}}).skip((page-1)*per_page).limit(per_page)
                                 )
         pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         return render_template('listings.html', listings=listings_data, pagination=pagination)
     return redirect(url_for('login'))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
