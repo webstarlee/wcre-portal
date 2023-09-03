0762d7eadf4e898b5337ea978c02cded1755db3f
Nate W <54341514+natew100@users.noreply.github.com>
2023-09-02 22:50:10 -0400

    sales edit + optimization revamp

--- static/js/sales.js
+++ static/js/sales.js
@@ -2,85 +2,36 @@ $(document).ready(function() {
 	var fileInput = document.getElementById("sale-agreement");
 	var uploadButton = document.getElementById("upload-sale-agreement");
 	const buyerPhoneInput = document.getElementById("sale-buyer-phone");
-	buyerPhoneInput.addEventListener("input", formatBuyerPhoneNumber);
+	const editBuyerPhoneInput = document.getElementById("edit-sale-buyer-phone");
 	const sellerPhoneInput = document.getElementById("sale-seller-phone");
-	sellerPhoneInput.addEventListener("input", formatSellerPhoneNumber);
+	const editSellerPhoneInput = document.getElementById("edit-sale-seller-phone");
+	buyerPhoneInput.addEventListener("input", () => formatPhoneNumber(buyerPhoneInput));
+	editBuyerPhoneInput.addEventListener("input", () => formatPhoneNumber(editBuyerPhoneInput));
+	sellerPhoneInput.addEventListener("input", () => formatPhoneNumber(sellerPhoneInput));
+	editSellerPhoneInput.addEventListener("input", () => formatPhoneNumber(editSellerPhoneInput));
 	const salePriceInput = document.getElementById("sale-price");
-	salePriceInput.addEventListener("input", formatSalePrice);
 	const editSalePriceInput = document.getElementById("edit-sale-price");
-	editSalePriceInput.addEventListener("input", formatSalePriceEdit);
+	salePriceInput.addEventListener("input", () => formatPrice(salePriceInput));
+	editSalePriceInput.addEventListener("input", () => formatPrice(editSalePriceInput));
 	const sqFootageInput = document.getElementById("sale-sqft");
-	sqFootageInput.addEventListener("input", formatSquareFootage);
 	const editsqFootageInput = document.getElementById("edit-sale-sqft");
-	editsqFootageInput.addEventListener("input", formatSquareFootageEdit);
+	sqFootageInput.addEventListener("input", () => formatSqFootage(sqFootageInput));
+	editsqFootageInput.addEventListener("input", () => formatSqFootage(editsqFootageInput));
 
-	function showErrorNotification(message) {
-		var errorNotification = $("#error-notification");
-		errorNotification.text(message);
-		errorNotification.addClass("show");
+
+	function showNotification(message, elementId) {
+		var notification = $("#" + elementId);
+		notification.text(message);
+		notification.addClass("show");
 		setTimeout(function() {
-			errorNotification.removeClass("show");
+			notification.removeClass("show");
 		}, 2000);
 	}
 
-	function showSuccessNotification(message) {
-		var successNotification = $("#success-notification");
-		successNotification.text(message);
-		successNotification.addClass("show");
-		setTimeout(function() {
-			successNotification.removeClass("show");
-		}, 2000);
-	}
-
-	function showSuccessNotificationModal(message) {
-		var successNotification = $("#success-notification-modal");
-		successNotification.text(message);
-		successNotification.addClass("show");
-		setTimeout(function() {
-			successNotification.removeClass("show");
-		}, 2000);
-	}
-
-	function showErrorNotificationModal(message) {
-		var errorNotification = $("#error-notification-modal");
-		errorNotification.text(message);
-		errorNotification.addClass("show");
-		setTimeout(function() {
-			errorNotification.removeClass("show");
-		}, 2000);
-	}
-
-	function formatSalePrice() {
-		let inputText = salePriceInput.value.trim();
+	function formatPrice(inputElement) {
+		let inputText = inputElement.value.trim();
 		if (isNaN(inputText.replace(/[,.$]/g, ""))) {
-			salePriceInput.value = inputText;
-			return;
-		}
-		let numericValue = salePriceInput.value.replace(/[^0-9.,]/g, "");
-		numericValue = numericValue.replace(/\.+/g, ".").replace(/,+/g, ",");
-		const parts = numericValue.split(".");
-		if (parts.length > 1) {
-			parts[1] = parts[1].substring(0, 2);
-			numericValue = parts.join(".");
-		}
-		let cents = "";
-		if (numericValue.includes(".")) {
-			[numericValue, cents] = numericValue.split(".");
-			cents = "." + cents;
-		}
-		numericValue = numericValue.replace(/,/g, "");
-		const numberValue = isNaN(parseFloat(numericValue)) ?
-			0 :
-			parseFloat(numericValue);
-		const formattedNumber = new Intl.NumberFormat("en-US").format(numberValue);
-		const formattedPrice = numberValue ? `$${formattedNumber}${cents}` : "";
-		salePriceInput.value = formattedPrice;
-	}
-
-	function formatSalePriceEdit() {
-		let inputText = editSalePriceInput.value.trim();
-		if (isNaN(inputText.replace(/[,.$]/g, ""))) {
-			editSalePriceInput.value = inputText;
+			inputElement.value = inputText;
 			return;
 		}
 
@@ -91,88 +42,53 @@ $(document).ready(function() {
 			parts[1] = parts[1].substring(0, 2);
 			numericValue = parts.join(".");
 		}
+
 		let cents = "";
 		if (numericValue.includes(".")) {
 			[numericValue, cents] = numericValue.split(".");
 			cents = "." + cents;
 		}
 		numericValue = numericValue.replace(/,/g, "");
-		const numberValue = isNaN(parseFloat(numericValue)) ?
-			0 :
-			parseFloat(numericValue);
+		const numberValue = isNaN(parseFloat(numericValue)) ? 0 : parseFloat(numericValue);
 		const formattedNumber = new Intl.NumberFormat("en-US").format(numberValue);
 		const formattedPrice = numberValue ? `$${formattedNumber}${cents}` : "";
-		editSalePriceInput.value = formattedPrice;
+		inputElement.value = formattedPrice;
 	}
 
-	function formatSquareFootage() {
-		let numericValue = sqFootageInput.value.replace(/[^0-9.,]/g, "");
-		numericValue = numericValue.replace(/\.+/g, ".").replace(/,+/g, ",");
-		const parts = numericValue.split(".");
-		if (parts.length > 1) {
-			parts[1] = parts[1].substring(0, 2);
-			numericValue = parts.join(".");
-		}
-		let cents = "";
-		if (numericValue.includes(".")) {
-			[numericValue, cents] = numericValue.split(".");
-			cents = "." + cents;
-		}
-		numericValue = numericValue.replace(/,/g, "");
-		const numberValue = isNaN(parseFloat(numericValue)) ?
-			0 :
-			parseFloat(numericValue);
-		const formattedNumber = new Intl.NumberFormat("en-US").format(numberValue);
-		const formattedSqft = numberValue ? `${formattedNumber}${cents}` : "";
-		sqFootageInput.value = formattedSqft;
-	}
-
-	function formatSquareFootageEdit() {
-		let numericValue = editsqFootageInput.value.replace(/[^0-9.,]/g, "");
-		numericValue = numericValue.replace(/\.+/g, ".").replace(/,+/g, ",");
-		const parts = numericValue.split(".");
-		if (parts.length > 1) {
-			parts[1] = parts[1].substring(0, 2);
-			numericValue = parts.join(".");
-		}
-		let cents = "";
-		if (numericValue.includes(".")) {
-			[numericValue, cents] = numericValue.split(".");
-			cents = "." + cents;
-		}
-		numericValue = numericValue.replace(/,/g, "");
-		const numberValue = isNaN(parseFloat(numericValue)) ?
-			0 :
-			parseFloat(numericValue);
-		const formattedNumber = new Intl.NumberFormat("en-US").format(numberValue);
-		const formattedSqft = numberValue ? `${formattedNumber}${cents}` : "";
-		editsqFootageInput.value = formattedSqft;
-	}
-
-	function formatBuyerPhoneNumber() {
-		let phoneNumber = buyerPhoneInput.value.replace(/\D/g, "");
+	function formatPhoneNumber(inputElement) {
+		let phoneNumber = inputElement.value.replace(/\D/g, "");
 		if (phoneNumber.length > 10) {
 			phoneNumber = phoneNumber.substr(0, 10);
 		}
 		const formattedPhoneNumber = formatToPhoneNumber(phoneNumber);
-		buyerPhoneInput.value = formattedPhoneNumber;
-	}
-
-	function formatSellerPhoneNumber() {
-		let phoneNumber = sellerPhoneInput.value.replace(/\D/g, "");
-		if (phoneNumber.length > 10) {
-			phoneNumber = phoneNumber.substr(0, 10);
-		}
-		const formattedPhoneNumber = formatToPhoneNumber(phoneNumber);
-		sellerPhoneInput.value = formattedPhoneNumber;
+		inputElement.value = formattedPhoneNumber;
 	}
 
 	function formatToPhoneNumber(phoneNumber) {
-		const formattedNumber = phoneNumber.replace(
-			/(\d{3})(\d{3})(\d{4})/,
-			"$1-$2-$3"
-		);
+		const formattedNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
 		return formattedNumber;
+	}
+
+	function formatSqFootage(inputElement) {
+		let numericValue = inputElement.value.replace(/[^0-9.,]/g, "");
+		numericValue = numericValue.replace(/\.+/g, ".").replace(/,+/g, ",");
+
+		const parts = numericValue.split(".");
+		if (parts.length > 1) {
+			parts[1] = parts[1].substring(0, 2);
+			numericValue = parts.join(".");
+		}
+
+		let cents = "";
+		if (numericValue.includes(".")) {
+			[numericValue, cents] = numericValue.split(".");
+			cents = "." + cents;
+		}
+		numericValue = numericValue.replace(/,/g, "");
+		const numberValue = isNaN(parseFloat(numericValue)) ? 0 : parseFloat(numericValue);
+		const formattedNumber = new Intl.NumberFormat("en-US").format(numberValue);
+		const formattedSqft = numberValue ? `${formattedNumber}${cents}` : "";
+		inputElement.value = formattedSqft;
 	}
 
 	$(function() {
@@ -215,12 +131,32 @@ $(document).ready(function() {
 		const saleSqFt = selectedRow.find("td:nth-child(5)").text().trim();
 		const saleStreet = selectedRow.find("td:nth-child(7)").text().trim();
 		const saleCity = selectedRow.find("td:nth-child(8)").text().trim();
+		const saleBuyer = selectedRow.find("td:nth-child(9)").text().trim();
+		const saleBuyerEmail = selectedRow
+			.find("td:nth-child(9) a")
+			.attr("href")
+			.replace("mailto:", "")
+			.trim();
+		const saleBuyerPhone = selectedRow.find("td:nth-child(10)").text().trim();
+		const saleSeller = selectedRow.find("td:nth-child(11)").text().trim();
+		const saleSellerEmail = selectedRow
+			.find("td:nth-child(11) a")
+			.attr("href")
+			.replace("mailto:", "")
+			.trim();
+		const saleSellerPhone = selectedRow.find("td:nth-child(12)").text().trim();
 		$("#edit-sale-type").val(saleType);
 		$("#edit-sale-end-date").val(saleClosingDate);
 		$("#edit-sale-price").val(salePrice);
 		$("#edit-sale-sqft").val(saleSqFt);
 		$("#edit-sale-street").val(saleStreet);
 		$("#edit-sale-city").val(saleCity);
+		$("#edit-sale-buyer-name").val(saleBuyer);
+		$("#edit-sale-buyer-email").val(saleBuyerEmail);
+		$("#edit-sale-buyer-phone").val(saleBuyerPhone);
+		$("#edit-sale-seller-name").val(saleSeller);
+		$("#edit-sale-seller-email").val(saleSellerEmail);
+		$("#edit-sale-seller-phone").val(saleSellerPhone);
 	});
 
 	// CLOSE ADD SALE MODAL
@@ -342,7 +278,7 @@ $(document).ready(function() {
 					textStatus,
 					errorThrown
 				);
-				showErrorNotificationModal("Error Fetching Search Results");
+				showNotification("Error Fetching Search Results", "error-notification-modal");
 			},
 		});
 	}
@@ -405,6 +341,12 @@ $(document).ready(function() {
 					var saleSqFt = $("#edit-sale-sqft").val();
 					var saleStreet = $("#edit-sale-street").val();
 					var saleCity = $("#edit-sale-city").val();
+					var saleBuyer = $("#edit-sale-buyer-name").val();
+					var saleBuyerEmail = $("#edit-sale-buyer-email").val();
+					var saleBuyerPhone = $("#edit-sale-seller-phone").val();
+					var saleSeller = $("#edit-sale-seller-name").val();
+					var saleSellerEmail = $("#edit-sale-seller-email").val();
+					var saleSellerPhone = $("#edit-sale-seller-phone").val();
 
 					var data = {
 						sale_type: saleType,
@@ -413,6 +355,12 @@ $(document).ready(function() {
 						sale_sqft: saleSqFt,
 						sale_street: saleStreet,
 						sale_city: saleCity,
+						sale_buyer: saleBuyer,
+						sale_buyer_email: saleBuyerEmail,
+						sale_buyer_phone: saleBuyerPhone,
+						sale_seller: saleSeller,
+						sale_seller_email: saleSellerEmail,
+						sale_seller_phone: saleSellerPhone
 					};
 
 					$.ajax({
@@ -425,7 +373,7 @@ $(document).ready(function() {
 								console.log("Sale Edited Successfully");
 								location.reload();
 							} else {
-								showErrorNotificationModal("Error Editing Sale");
+								showNotification("Error Editing Sale", "error-notification-modal");
 								console.log("Error Editing Sale");
 							}
 						},
@@ -435,11 +383,11 @@ $(document).ready(function() {
 						},
 					});
 				} else {
-					showErrorNotificationModal("Please Fill Out All Required Fields");
+					showNotification("Please Fill Out All Required Fields", "error-notification-modal");
 				}
 			}
 		} else {
-			showErrorNotificationModal("Please Fill Out All Required Fields");
+			showNotification("Please Fill Out All Required Fields", "error-notification-modal");
 		}
 	});
 
@@ -477,16 +425,16 @@ $(document).ready(function() {
 			success: function(data) {
 				if (data.success) {
 					uploadButton.textContent = "Document Uploaded ✔";
-					showSuccessNotificationModal("Document Uploaded Successfully");
+					showNotification("Document Uploaded Successfully", "success-notification-modal");
 					uploadButton.disabled = true;
 					document.getElementById("sale-agreement-file-base64").value =
 						data.fileBase64;
 				} else {
-					showErrorNotificationModal("Error Uploading Document");
+					showNotification("Error Uploading Document", "error-notification-modal");
 				}
 			},
-			error: function(xhr, status, error) {
-				showErrorNotificationModal("Error Uploading Document");
+			error: function() {
+				showNotification("Error Uploading Document", "error-notification-modal");
 			},
 		});
 	});
@@ -536,7 +484,7 @@ $(document).ready(function() {
 			type: "GET",
 			success: function(data) {
 				if (data.success) {
-					showErrorNotification("Sale Deleted");
+					showNotification("Sale Deleted", "error-notification");
 					selectedRow.remove();
 					deleteModal.hide(); // hide the modal
 					console.log("Sale Deleted Successfully");
@@ -570,16 +518,14 @@ $(document).ready(function() {
 				})
 				.done(function(response, jqXHR) {
 					if (response.status === "success") {
-						showSuccessNotification("Sale Uploaded Successfully");
-						console.log("Sale Uploaded Successfully");
 						window.location.href = response.redirect;
 					} else {
-						showErrorNotification("Unexpected status code: " + jqXHR.status);
+						showNotification("Unexpected status code: " + jqXHR.status, "error-notification");
 						console.log("Unexpected status code: " + jqXHR.status);
 					}
 				})
 				.fail(function(textStatus, errorThrown) {
-					showErrorNotification("Error Uploading Sale");
+					showNotification("Error Uploading Sale", "error-notification");
 					console.log("Error Uploading Sale: ", textStatus, errorThrown);
 				});
 		}
