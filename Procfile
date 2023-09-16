a7d3753c7802f05c50be0d83a99eda9e62717164
Nate W <54341514+natew100@users.noreply.github.com>
2023-09-15 23:45:20 -0400

    .

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -c gunicorn_config.py app:app
+web: gunicorn app:app
--- gunicorn_config.py
+++
@@ -1,5 +0,0 @@
-import sys
-accesslog = "-"
-errorlog = "-"
-loglevel = "info"  # You can adjust the log level as needed (e.g., "debug", "info", "warning", "error")
-workers = 2
